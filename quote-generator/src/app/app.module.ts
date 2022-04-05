6a2965998d27a11ab3bd60bc28bcd66d0d2928a2
Kisavi <deniskisavi@gmail.com>
2022-04-05 18:34:25 +0300

    generate quote class, quote and quote item components

--- quote-generator/src/app/app.module.ts
+++ quote-generator/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { QuoteItemComponent } from './content/quote-item/quote-item.component';
+import { QuoteComponent } from './content/quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    QuoteItemComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
