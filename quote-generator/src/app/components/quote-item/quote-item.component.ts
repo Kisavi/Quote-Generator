661c989814b9cb8a3eafcab3653ca383e46e68c4
Kisavi <deniskisavi@gmail.com>
2022-04-12 13:46:59 +0300

    style the highlight for the quote with highest upvote

--- quote-generator/src/app/components/quote-item/quote-item.component.ts
+++ quote-generator/src/app/components/quote-item/quote-item.component.ts
@@ -11,13 +11,41 @@ export class QuoteItemComponent implements OnInit {
   @Input() 'quote': Quote;
   @Output() isComplete = new EventEmitter<boolean>();
 
+//downvote function
   quoteDownVote() {
     this.quote.downVote -=1;
   }
 
+  // upvote function
   quoteUpVote() {
     this.quote.upVote +=1;
   }
+
+  // // highlighting quote with highest upvotes
+  // inspirationalQuote() {
+  //   // Step 1: here we check and return the highest upvote value/score
+  //   let maxValue: number = Math.max.apply(
+  //     Math,
+  //     this.quotes.map(function (quote) {
+  //       return quote.upVote
+  //     })
+  //   );
+  //   //we can check to see the upvote with the highest value/score
+  //   console.log(maxValue);
+
+  //   //Step 2: here we find the index of the quote based on the maxValue we got above
+  //   let quoteIndex: number = this.quotes.findIndex(
+  //     (quote) => quote.upVote === maxValue
+  //   );
+  //   //we check the quote index that contains the highest number of upvotes using the maxValue we got above
+  //   console.log(quoteIndex);
+
+  //   //Step 3: Loop through all the quotes and set the value of isInspirational to true at index of quote with highest maxValue
+  //   this.quotes.forEach((quote) => {
+  //     quote.isInspirational = false;
+  //   });
+  //   this.quotes[quoteIndex].isInspirational = true;
+  // }
 
   constructor() { }
 
--- quote-generator/src/app/components/quote/quote.component.ts
+++ quote-generator/src/app/components/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Disastrous Quote', 'Sasa Mnataka nifanye nini Jameni...', 'Uhuru Kinyasa', 'Ali Baba', new Date(2022, 3, 1), 0, 0),
-    new Quote(1, 'Expressional Quote', 'Mtu kama huyo ni bure kabisaa! Ooh yes!', 'Niwai Kibangi', 'Chaggy Chan', new Date(2022, 3, 6), 0, 0),
-    new Quote(1, 'Happy Quote', 'Ukiwa na wakando hutaki kapicha, ukuwa na wako manze unachachisha...', 'The Other Delo', 'Magix Njenga', new Date(2022, 2, 28), 0, 0),
+    new Quote(1, 'Disastrous Quote', 'Sasa Mnataka nifanye nini Jameni...', 'Uhuru Kinyasa', 'Ali Baba', new Date(2022, 3, 1), 0, 0, false),
+    new Quote(1, 'Expressional Quote', 'Mtu kama huyo ni bure kabisaa! Ooh yes!', 'Niwai Kibangi', 'Chaggy Chan', new Date(2022, 3, 6), 0, 0, false),
+    new Quote(1, 'Happy Quote', 'Ukiwa na wakando hutaki kapicha, ukuwa na wako manze unachachisha...', 'The Other Delo', 'Magix Njenga', new Date(2022, 2, 28), 0, 0, false),
   ]
 
   //function to add new quote generated from the form
@@ -20,7 +20,7 @@ export class QuoteComponent implements OnInit {
     quote.id = quoteLength + 1;
     this.quotes.unshift(quote)
   }
- 
+
   //function to delete quote when delete button is pressed
   deletQuote(index: any) {
     let result = confirm(`Are you sure you want to delete this quote by ${this.quotes[index].author}?`)
@@ -32,6 +32,32 @@ export class QuoteComponent implements OnInit {
   //function of toggling the quote details
   toggleDetails(index: any) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  // // highlighting quote with highest upvotes
+  inspirationalQuote() {
+    // Step 1: here we check and return the highest upvote value/score
+    let maxValue: number = Math.max.apply(
+      Math,
+      this.quotes.map(function (quote) {
+        return quote.upVote
+      })
+    );
+    //we can check to see the upvote with the highest value/score
+    console.log(maxValue);
+
+    //Step 2: here we find the index of the quote based on the maxValue we got above
+    let quoteIndex: number = this.quotes.findIndex(
+      (quote) => quote.upVote === maxValue
+    );
+    //we check the quote index that contains the highest number of upvotes using the maxValue we got above
+    console.log(quoteIndex);
+
+    //Step 3: Loop through all the quotes and set the value of isInspirational to true at index of quote with highest maxValue
+    this.quotes.forEach((quote) => {
+      quote.isInspirational = false;
+    });
+    this.quotes[quoteIndex].isInspirational = true;
   }
 
   constructor() { }
